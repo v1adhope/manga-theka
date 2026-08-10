6e16cad1e5eacfcde8ed153bda7a100a531dccd5
v1adhope <vladislavgardner@proton.me>
2026-08-10 14:35:29 +0300

    Fix migrations

--- migrations/20260629101118_add_labels.up.sql
+++ migrations/20260629101118_add_labels.up.sql
@@ -1,9 +1,9 @@
 create table if not exists labels (
 	id uuid,
 	name text not null,
-	type uuid not null,
+	type text not null,
 
 	constraint pk_labels_id primary key(id),
 	constraint check_length_labels_name check(char_length(name) <= 255),
-	constraint fk_labels_label_types_type foreign key(type) references label_types(id)
+	constraint enum_labels_type check(type in ('Genre', 'Tag'))
 );
